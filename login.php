653f1dd39adb50dd46d63e8b652230be516c5691
JordanTan722 <103820790+JordanTan722@users.noreply.github.com>
2022-04-30 13:54:17 +0800

    Controller and Entity
    *Added new class LoginController.
    *Reworked some functions

--- login.php
+++ login.php
@@ -1,78 +1,71 @@
 <?php 
-
+// Run when submit (Main Function)
 if (isset($_POST['submit'])) {
-  redirect();
+  $main = new LoginController();
+  $main->redirect();
 }
 
-function validate() {
+class LoginController {
   // Declare variables
-  $Message = "";
-  $ErrorUname = "";
-  $sqlRole = "";
-  $sqlUsername = "";
-  $sqlPassword = "";
+  private $message = "";
+  private $sqlRole = "";
+  private $sqlUsername = "";
+  private $sqlPassword = "";
 
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Receive Data
+  // Validate Information with database
+  public function validate($sqlRole, $sqlUsername, $sqlPassword) {
+    include('conn.php');
+
+    $query=mysqli_query($conn,"select * from users where role = '$sqlRole' && username ='$sqlUsername' && password ='$sqlPassword' && isActivate ='Y'");
+    $num_rows=mysqli_num_rows($query);
+    $row=mysqli_fetch_array($query);
+
+    // If there is valid data
+    if ($num_rows > 0) {
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  // Redirect Page
+  public function redirect() {
     $sqlRole = $_POST['role'];
     $sqlUsername = $_POST['username'];
     $sqlPassword = $_POST['password'];
 
-    // If Error occurred
-    if ($ErrorUname!=""){
-      $Message = "Login failed! Errors found";
+    // Call validate function
+    if ($this->validate($sqlRole, $sqlUsername, $sqlPassword) == true) {
+      switch($sqlRole) {
+        case "Owner": {
+          header("Location:Owner.html");
+          break;
+        }
+        case "Admin": {
+          header("Location:Administrator.html");
+          break;
+        }
+        case "Manager": {
+          header("Location:Restaurant_Manager.html");
+          break;
+        }
+        case "Staff": {
+          header("Location:Restaurant_Staff.html");
+          break;
+        }
+        case "Customer": {
+          header("Location:Customer.html");
+          break;
+        }
+        default:
+          header("Location:login.html");
+      }
     }
-    else{
-      // Connect Database
-      include('conn.php');
-
-      $query=mysqli_query($conn,"select * from users where role = '$sqlRole' && username ='$sqlUsername' && password ='$sqlPassword'");
-      $num_rows=mysqli_num_rows($query);
-      $row=mysqli_fetch_array($query);
-
-      // If there is valid data
-      if ($num_rows > 0){
-        return true;
-      }
-      else{
-        return false;
-      }
+    else {
+      echo nl2br ("<h2>Login Failed. \nRedirecting to Login Page...</h2>");
+      header("refresh:2;url=login.html");
     }
-  }
-}
-    
-function redirect() {
-  $sqlRole = $_POST['role'];
-
-  if (validate() == true) {
-    switch($sqlRole) {
-      case "Owner": {
-        header("Location:Owner.html");
-        break;
-      }
-      case "Admin": {
-        header("Location:Administrator.html");
-        break;
-      }
-      case "Manager": {
-        header("Location:Restaurant_Manager.html");
-        break;
-      }
-      case "Staff": {
-        header("Location:Restaurant_Staff.html");
-        break;
-      }
-      case "Customer": {
-        header("Location:Customer.html");
-        break;
-      }
-      default:
-        header("Location:login.html");
-    }
-  }
-  else {
-    echo nl2br ("<h2 style='top:50%; left:50%;'>Login Failed. \nRedirecting to Login Page...</h2>");
-    header("refresh:2;url=login.html");
   }
 }
 ?>
