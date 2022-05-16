fc57335bd6b5ff45b62b55152fe59e854e24fef7
joseph00707 <104149381+joseph00707@users.noreply.github.com>
2022-05-16 19:05:57 +0800

    Add files via upload

---
+++ myrestaurant.php
@@ -0,0 +1,105 @@
+<?php
+include('session_m.php');
+
+if(!isset($login_session)){
+header('Location: managerlogin.php'); // Redirecting To Home Page
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title> Manager Login | Break First </title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/myrestaurant.css">
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <body>
+
+
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+  
+    <script type="text/javascript">
+      window.onscroll = function()
+      {
+        scrollFunction()
+      };
+
+      function scrollFunction(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+        } else {
+          document.getElementById("myBtn").style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Break First</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $login_session; ?> </a></li>
+            <li class="active"> <a href="managerlogin.php">MANAGER CONTROL PANEL</a></li>
+            <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+
+
+
+
+<div class="container">
+    <div class="jumbotron">
+     <h1>Hello Manager! </h1>
+     <p>Manage Your Restuarant Here!</p>
+
+    </div>
+    </div>
+<div class="container">
+    <div class="container">
+    	<div class="col">
+    		
+    	</div>
+    </div>
+
+    
+    	<div class="col-xs-3" style="text-align: center;">
+
+    	<div class="list-group">
+    		<a href="view_food_items.php" class="list-group-item ">View Menu Items</a>
+    		<a href="add_food_items.php" class="list-group-item ">Create Menu Items</a>
+    		<a href="edit_food_items.php" class="list-group-item ">Update Menu Items</a>
+    		<a href="delete_food_items.php" class="list-group-item ">Delete Menu Items</a>
+    	</div>
+    </div>        
+        </div>
+      
+    </div>
+</div>
+
+  </body>
+</html>
